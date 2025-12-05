e946ddee0b96e5d38c2e980ddc8ff5aba0670e80
Antoniodigiovanni <antoniodg98@gmail.com>
2025-12-05 21:09:23 +0100

    updates config

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,12 +1,4 @@
 import os
-
-# MONGO_URI = os.getenv("MONGO_URI")
-# DB_NAME = "vault"
-# COLLECTION_NAME = "obsidian_vault"
-# INDEX_NAME = "vector_index"
-# OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-
-import os
 from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
 
@@ -14,11 +6,11 @@ load_dotenv()
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Obsidian Vault RAG"
-    API_V1_STR: str = "/api/v1"
 
     MONGO_URI: str = os.getenv("MONGO_URI")
-    DB_NAME: str = "vault"
-    COLLECTION_NAME: str = "obsidian_vault"
+    DB_NAME: str = "obsidian_rag"
+    CHUNKS_COLLECTION_NAME: str = "chunks"
+    DOCUMENTS_COLLECTION_NAME: str = "documents"
     INDEX_NAME: str = "vector_index"
     OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY")
 
